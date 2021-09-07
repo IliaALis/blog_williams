74a50b00d952d177519301885e01c75d9980dbf9
IlLi <51474097+IliaALis@users.noreply.github.com>
2021-09-07 22:16:40 +0300

    Update test.py

--- blog/test.py
+++ blog/test.py
@@ -38,7 +38,7 @@ class BlogTests(TestCase):
         
     def test_post_detail_view(self):
         response = self.client.get('/post/1/')
-        no_response = self.client.get('/post/100000')
+        no_response = self.client.get('/post/100000/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
